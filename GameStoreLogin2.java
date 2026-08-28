dc5283efda985e8f5536db5882763cb77aa1d339
VaniAri <xvaniacruzariasx@gmail.com>
2026-08-28 16:43:31 -0500

    Create GameStoreLogin2.java

---
+++ GameStoreLogin2.java
@@ -0,0 +1,59 @@
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener; 
+
+public class GameStoreLogin2 extends JFrame {
+    private JLabel lblTitulo, lblUsuario;
+    private JTextField txtUsuario;
+    private JButton btnIngresar;
+
+    public GameStoreLogin2() {
+        setTitle("GameStore Escolar - Acceso");
+        setSize(350, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setLayout(null);
+
+        lblTitulo = new JLabel("BIENVENIDO A GAMESTORE");
+        lblTitulo.setBounds(70, 20, 200, 25);
+        add(lblTitulo);
+
+        lblUsuario = new JLabel("Usuario:");
+        lblUsuario.setBounds(40, 70, 80, 25);
+        add(lblUsuario);
+
+        txtUsuario = new JTextField();
+        txtUsuario.setBounds(110, 70, 180, 25);
+        add(txtUsuario);
+
+        btnIngresar = new JButton("Ingresar");
+        btnIngresar.setBounds(110, 110, 100, 30);
+        add(btnIngresar);
+
+       
+        btnIngresar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               
+                String usuarioIngresado = txtUsuario.getText();
+
+                
+                if (!usuarioIngresado.isEmpty()) {
+                    JOptionPane.showMessageDialog(null,
+                        "¡Acceso concedido! Bienvenido, " + usuarioIngresado + ".",
+                        "GameStore", JOptionPane.INFORMATION_MESSAGE);
+                } else {
+                    JOptionPane.showMessageDialog(null,
+                        "Por favor, ingrese un nombre de usuario.",
+                        "Advertencia", JOptionPane.WARNING_MESSAGE);
+                }
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        new GameStoreLogin2().setVisible(true);
+    }
+}
+
